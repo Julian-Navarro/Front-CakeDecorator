f71a11d7c43ad5317277fd4eb429b36103b1b76a
Julian Navarro <julianrodrigonavarro@gmail.com>
2023-04-22 07:38:35 -0300

    Fixes

--- src/components/Shop/LifeSideBar/LeftSideBar.jsx
+++ src/components/Shop/LifeSideBar/LeftSideBar.jsx
@@ -8,9 +8,13 @@ import { HOST } from "../../../utils"
 export default function LeftSideBar({ handlerSetProducts, isOpen }) {
     // const [isOpen, setIsOpen] = useState(false);
     let [productsCategories, setProductsCategories] = useState([]);
+    let [brands, setBrands] = useState([]);
     let [showCategoriesFlag, setShowCategoriesFlag] = useState(false);
     let [showBrandsFlag, setShowBrandsFlag] = useState(false);
-
+    async function getBrands() {
+        const brandsDB = await axios.get(`${HOST}/brands`)
+        setBrands(brandsDB.data)
+    }
     async function getCategories(){
         const productsCategoriesDB = await axios.get(`${HOST}/categories`)
         setProductsCategories(productsCategoriesDB.data)
@@ -18,6 +22,7 @@ export default function LeftSideBar({ handlerSetProducts, isOpen }) {
     
     useEffect(()=>{
         getCategories()
+        getBrands()
         console.log("showCategoriesFlag: ",showCategoriesFlag);
     },[showCategoriesFlag])
     return (
@@ -26,17 +31,24 @@ export default function LeftSideBar({ handlerSetProducts, isOpen }) {
             {
                 productsCategories.length && isOpen
                 ? <Div flexDir="column">
-                    <Ul jfCont="flex-start"wd="100%"fSize="17px"_hovCol="#fff"txtSh="#fff"color="lightgray"ml="20px"hg="2rem"onClick={()=>setShowCategoriesFlag(!showCategoriesFlag)}> <P color="lightgray" >Categorias</P> <P color="lightgray" fSize="16px">{showCategoriesFlag?"◄":"►"}</P> </Ul>
-                    {
-                    showCategoriesFlag ?
-                    <Div flexDir="column"alItems="flex-start"ml="70px"wd="100%">
+                    <Ul jfCont="flex-start"wd="100%"fSize="17px"_hovCol="#fff"txtSh="#fff"color="lightgray"ml="20px"hg="2rem"onClick={()=>setShowCategoriesFlag(!showCategoriesFlag)}> 
+                      <P color="lightgray" >Categorias</P> 
+                      <P color="lightgray" fSize="16px">{showCategoriesFlag?"◄":"►"}</P>
+                    </Ul>
+
+                   { 
+                   showCategoriesFlag 
+                  ? <Div flexDir="column"alItems="flex-start"ml="70px"wd="100%">
                       <Li onClick={()=>handlerSetProducts("default")} bg="transparent"bd="transparent"fSize="16px"color="lightgray"_hovCol="#fff"txtSh="#fff">Todas</Li>
                       {productsCategories.map((cat) => 
                       <Li onClick={()=>handlerSetProducts(cat.category)} color="lightgray"_hovCol="#fff"txtSh="#fff"key={cat.id} bg="transparent"bd="transparent"fSize="14px">{cat.category}</Li>)}
                     </Div>
                     : null    
                     }
-                    <Ul fSize="17px"_hovCol="#fff"txtSh="#fff"color="lightgray"ml="20px"hg="2rem"jfCont="flex-start"wd="100%"> <P color="lightgray">Marcas</P> <P color="lightgray" fSize="16px">{showBrandsFlag?"◄":"►"}</P></Ul>
+                    <Ul fSize="17px"_hovCol="#fff"txtSh="#fff"color="lightgray"ml="20px"hg="2rem"jfCont="flex-start"wd="100%">
+                      <P color="lightgray">Marcas</P>
+                      <P color="lightgray" fSize="16px">{showBrandsFlag?"◄":"►"}</P>
+                    </Ul>
                 </Div>
                 : null
             }
