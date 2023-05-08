1073618624949d67b43bb14323ef53555f179473
Julian Navarro <julianrodrigonavarro@gmail.com>
2023-05-08 16:26:50 -0300

    Refactor CSS Navbar & Shop left side bar

--- src/components/Shop/LifeSideBar/LeftSideBar.jsx
+++ src/components/Shop/LifeSideBar/LeftSideBar.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react"
-import { Div, P, Ul, Li, Button, Img } from "../../../utils/StyledComponents/StyledComponents"
-import { HamburgerIcon, CloseIcon, ArrowRightIcon, ArrowLeftIcon } from "@chakra-ui/icons";
-import { IconButton, filter } from "@chakra-ui/react"
-import { HiMenu } from "react-icons/hi"
-import { useEffect } from "react";
-import axios from "axios";
 import { HOST } from "../../../utils"
+import { Div, P, Ul, Button } from "../../../utils/StyledComponents/StyledComponents"
+import { useState } from "react"
+import { useEffect } from "react";
+import { HiMenu } from "react-icons/hi"
 import { useNavigate } from "react-router-dom";
 import { MdShoppingCart } from "react-icons/md"
+import { ArrowRightIcon, ArrowLeftIcon } from "@chakra-ui/icons";
+import axios from "axios";
 import s from "../../../utils/example.module.css"
 
 
@@ -17,6 +16,8 @@ export default function LeftSideBar({ handlerSetFilters, isOpen ,setIsOpen, filt
     let [brands, setBrands] = useState([]);
     let [showCategoriesFlag, setShowCategoriesFlag] = useState(false);
     let [showBrandsFlag, setShowBrandsFlag] = useState(false);
+    const red = "#dc4a61"
+
     async function getBrands() {
         const brandsDB = await axios.get(`${HOST}/brands`)
         setBrands(brandsDB.data)
@@ -25,10 +26,6 @@ export default function LeftSideBar({ handlerSetFilters, isOpen ,setIsOpen, filt
         const productsCategoriesDB = await axios.get(`${HOST}/categories`)
         setProductsCategories(productsCategoriesDB.data)
     } 
-    const pink = "#ddc6da"
-    const violet = "#dddbe8"
-    const lightblue = "#7bb4fa"
-    const orange="rgb(255, 190, 70)"
     useEffect(()=>{
         getCategories()
         getBrands()
@@ -39,74 +36,82 @@ export default function LeftSideBar({ handlerSetFilters, isOpen ,setIsOpen, filt
             wd={isOpen?"14rem":"3.2rem"}
             hg="100vh" bg="transparent"overflow="hidden"
             pos="absolute"posTop="0rem"flexDir="column" br="0 12px 0 0"pd="0rem 0rem 0rem 0rem"
-            boxSh=".1rem .1rem .4rem .2rem #333"
+            boxSh=".1rem .1rem .4rem .2rem #252525"
             jfCont="flex-start"
             >
           <div 
           className={s.divGradient}
           bg="red" flexDir="column"wd="100%"hg="100%"jfCont="flex-start">
             <Div flexDir="column"mt="1rem">
-                <IconButton 
-                    icon={isOpen ? <ArrowLeftIcon color="#333"/> : <HiMenu color="#333"/>}
+                <Button 
                     onClick={()=>isOpen ? setIsOpen(false) : setIsOpen(true)}
-                    w="2.4rem"
-                    h="2.4rem"
-                    borderRadius="100%"
+                    wd="2.4rem"
+                    hg="2.4rem"
+                    br="100%"
                     cursor={"pointer"}
-                    alignSelf={"flex-start"}
-                    marginLeft={".3rem"}
-                    className={s.icons}  
-                    marginBottom={".3rem"}
-                    fontSize={"1.6rem"}
-                    bg={"lightgray"}
-                    />  
-                <IconButton 
-                    icon={<MdShoppingCart color="#333"/>}
+                    alSelf={"flex-start"}
+                    ml={".3rem"}
+                    // className={s.icons}  
+                    mb={".3rem"}
+                    fSize={"1.6rem"}
+                    bg={"#252525"}
+                    _hovBg="#dc4a61"
+                    boxSh="2px 2px .2rem .05rem #333"
+                    >
+                    {isOpen ? <ArrowLeftIcon color="#eeee"/> : <HiMenu color="#eeee"/>}  
+                </Button>  
+                <Button 
                     onClick={()=>navigate("/shop/cart")}
-                    w="2.4rem"
-                    h="2.4rem"
-                    fontSize={"1.6rem"}
-                    borderRadius="100%"
+                    ww="2.4rem"
+                    hh="2.4rem"
+                    fSize={"1.6rem"}
+                    br="100%"
                     cursor={"pointer"}
-                    alignSelf={"flex-start"}
-                    marginLeft={".3rem"}
-                    marginBottom={".3rem"}
-                    bg={"lightgray"}
-                    className={s.icons}
-                    />                
+                    alSelf={"flex-start"}
+                    ml={".3rem"}
+                    mb={".3rem"}
+                    bg={"#252525"}
+                    boxSh="2px 2px .2rem .05rem #333"
+                    _hovBg="#dc4a61"
+                    > 
+                      <MdShoppingCart color="#eeee"/>
+                </Button>               
             </Div>
             <Div flexDir="column"wd="100%"pos="relative"posLeft={isOpen?"0":"-8rem"}>
             {
                 productsCategories.length
-                ? <Div flexDir="column"wd="100%">
-                    <Ul jfCont="space-between"wd="100%"fSize="17px"
-                        color="#fff"ml="0px"hg="2rem"onClick={()=>setShowCategoriesFlag(!showCategoriesFlag)}> 
-                      <Div _hovBg={"rgba(0, 0, 0, 0.25)"}cursor="pointer">
-                        <P color={"#333"}fWeight="bold"wd="80%"jfCont="flex-start">Categorias</P> 
-                        <P color="lightgray"fSize="16px"wd="20%">{showCategoriesFlag?<ArrowLeftIcon fontWeight={"bold"}fontSize={"1rem"}color="#333"/>:<ArrowRightIcon fontWeight={"bold"}fontSize={"1rem"}color="#333"/>}</P>
+                ? 
+                <Div flexDir="column"wd="100%">
+                      <Div wd="95%"jfCont="space-between"_hovBg={"rgba(0, 0, 0, 0.05)"}cursor="pointer"_hovColIm="#dc4a61"onClick={()=>setShowCategoriesFlag(!showCategoriesFlag)}>
+                        <P color={"#252525"}fWeight="bold"wd="80%"jfCont="flex-start"_hovCol="#dc4a61">
+                          Categorias
+                        </P> 
+                        {showCategoriesFlag
+                        ? <ArrowLeftIcon fontWeight={"bold"}fontSize={"1rem"}color="#252525"marginRight=".15rem"/>
+                        : <ArrowRightIcon fontWeight={"bold"}fontSize={"1rem"}color="#252525"marginRight=".15rem"/>}
                       </Div>
-                    </Ul>
                    { 
                    showCategoriesFlag 
                    ? <Div flexDir="column"alItems="flex-start"ml="20px"wd="80%">
-                      <Button _hovBg={"rgba(0, 0, 0, 0.25)"}wd="100%"pd="0 0 0 .3rem"mt="3px"onClick={(e)=>handlerSetFilters(e)}value="all"
+                      <Button _hovBg={"rgba(0, 0, 0, 0.05)"}wd="100%"pd="0 0 0 .3rem"mt="3px"onClick={(e)=>handlerSetFilters(e)}value="all"
                         name="categories" bg="transparent"bd="transparent"fSize="18px"fWeight="bold"
-                        color={filters.categories.includes("all")?orange:"#333"}
-                        _hovCol={filters.categories.includes("all")?orange:"#252525"}
-                        // txtSh={filters.categories.includes("all")?"greenyellow":"lightgray"}
+                        color={filters.categories.includes("all")?red:"#252525"}
+                        _hovCol={filters.categories.includes("all")?red:"#252525"}
+                        // txtSh={filters.categories.includes("all")?"#252525":"#252525"}
                         jfCont="flex-start"
                         >Todas</Button>
 
                       {
                       productsCategories.map((cat) => 
                       <Button pd="0 0 0 .3rem"mt="3px"bg="transparent"name="categories"value={cat.category}mr="10px"
-                      color={filters.categories.includes(cat.category)?orange:"#333"}
-                      _hovCol={filters.categories.includes(cat.category)?orange:"#252525"}
-                      // txtSh={filters.categories.includes(cat.category)?lightblue:"lightgray"}
-                      _hovBg={"rgba(0, 0, 0, 0.25)"}wd="100%"jfCont="flex-start"fSize="18px"fWeight="bold"letterSp=".1rem"
-                      key={cat.id}
-                      onClick={(e)=>handlerSetFilters(e)}
-                      >{cat.category}</Button>)
+                        color={filters.categories.includes(cat.category)?red:"#252525"}
+                        _hovCol={filters.categories.includes(cat.category)?red:"#252525"}
+                        // txtSh={filters.categories.includes(cat.category)?lightblue:"lightgray"}
+                        _hovBg={"rgba(0, 0, 0, 0.05)"}wd="100%"jfCont="flex-start"fSize="18px"fWeight="bold"letterSp=".1rem"
+                        key={cat.id}
+                        onClick={(e)=>handlerSetFilters(e)}>
+                          {cat.category}
+                        </Button>)
                       }
                     </Div>
                     : null    
@@ -117,38 +122,39 @@ export default function LeftSideBar({ handlerSetFilters, isOpen ,setIsOpen, filt
             {
                 brands.length
                 ? <Div flexDir="column"wd="100%">
-                    <Ul jfCont="space-between"wd="100%"fSize="17px"_hovCol="#fff"color="lightgray"hg="2rem"onClick={()=>setShowBrandsFlag(!showBrandsFlag)}> 
-                    <Div _hovBg={"rgba(0 ,0 ,0 , 0.25)"}cursor="pointer">
-                      <P color="#252525" wd="80%"jfCont="flex-start"fWeight="bold">Marcas</P> 
-                      <P color="#252525" wd="20%"fSize="16px">{showBrandsFlag?<ArrowLeftIcon fontWeight={"bold"}fontSize={"1rem"}/>:<ArrowRightIcon fontWeight={"bold"}fontSize={"1rem"}/>}</P>
+                    <Div wd="95%"jfCont="space-between"_hovBg={"rgba(0, 0, 0, 0.05)"}cursor="pointer"onClick={()=>setShowBrandsFlag(!showBrandsFlag)}>
+                      <P color="#252525" wd="80%"jfCont="flex-start"fWeight="bold"_hovCol="#dc4a61">Marcas</P> 
+                      <P color="#252525" fSize="16px">
+                        {showBrandsFlag
+                        ?<ArrowLeftIcon fontWeight={"bold"}fontSize={"1rem"}/>
+                        :<ArrowRightIcon fontWeight={"bold"}fontSize={"1rem"}/>}
+                      </P>
                     </Div>
-                    </Ul>
                    { 
                    showBrandsFlag 
-                   ? <Div flexDir="column"alItems="flex-start"ml="20px"wd="80%">
-
+                   ? <Div flexDir="column"alItems="flex-start"ml="0px"wd="80%">
                       <Button pd="0 0 0 .3rem"mt=".1rem"bg="transparent"onClick={(e)=>handlerSetFilters(e)}name="brands"value="all"mr="10px"
-                        color={filters.brands.includes("all")?orange:"#252525"}
-                        _hovCol={filters.brands.includes("all")?orange:"#252525"}
+                        color={filters.brands.includes("all")?red:"#252525"}
+                        _hovCol={filters.brands.includes("all")?red:"#252525"}
                         // txtSh={filters.brands.includes("all")?"greenyellow":"lightgray"}
-                        _hovBg={"rgba(0 ,0 ,0 , 0.25)"}wd="100%"jfCont="flex-start"fSize="1rem"fWeight="bold"letterSp=".1rem"
+                        _hovBg={"rgba(0, 0, 0, 0.05)"}wd="100%"jfCont="flex-start"fSize="1rem"fWeight="bold"letterSp=".1rem"
                       >Todas</Button>
 
                       {
                       brands.map((br) => 
                       <Button pd="0 0 0 .3rem"mt=".1rem"bg="transparent"onClick={(e)=>handlerSetFilters(e)}name="brands"value={br.brand}mr="10px"
-                        color={filters.brands.includes(br.brand)?orange:"#252525"}
-                        _hovCol={filters.brands.includes(br.brand)?orange:"#252525"}
+                        color={filters.brands.includes(br.brand)?red:"#252525"}
+                        _hovCol={filters.brands.includes(br.brand)?red:"#252525"}
                         // txtSh={filters.brands.includes(br.brand)?"greenyellow":"lightgray"}
-                        _hovBg={"rgba(0 ,0 ,0 , 0.25)"}wd="100%"jfCont="flex-start"key={br.id}
+                        _hovBg={"rgba(0, 0, 0, 0.05)"}wd="100%"jfCont="flex-start"key={br.id}
                         fSize="1rem"fWeight="bold"letterSp=".1rem"
                         >{br.brand}</Button>)
                       }
                       <Button pd="0 0 0 .3rem"mt=".1rem"bg="transparent"onClick={(e)=>handlerSetFilters(e)}value={""}name="brands"
-                        color={filters.brands.includes("")?orange:"#252525"}
-                        _hovCol={filters.brands.includes("")?orange:"#252525"}
+                        color={filters.brands.includes("")?red:"#252525"}
+                        _hovCol={filters.brands.includes("")?red:"#252525"}
                         // txtSh={filters.brands.includes("")?"greenyellow":"lightgray"}
-                        _hovBg={"rgba(0 ,0 ,0 , 0.25)"}wd="100%"jfCont="flex-start"
+                        _hovBg={"rgba(0, 0, 0, 0.05)"}wd="100%"jfCont="flex-start"
                         fSize="1rem"fWeight="bold"letterSp=".1rem"
                       >Sin marca</Button>
 
@@ -160,7 +166,7 @@ export default function LeftSideBar({ handlerSetFilters, isOpen ,setIsOpen, filt
                 }
                 <Div wd="100%">
                 <Ul color={"#252525"}fWeight="bold"jfCont="space-between"wd="97%"fSize="22px"
-                _hovBg="rgba(0, 0, 0, 0.25)"hg="100%"
+                _hovBg="rgba(0, 0, 0, 0.05)"hg="100%"_hovCol="#dc4a61"
                 >
                     Condiciones de compra
                 </Ul>
